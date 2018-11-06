729342bf0b5b0a3722065042f827a43eecf2e145
induts <induts@gmail.com>
2018-11-05 16:09:59 -0800

    update 11/5 evening

--- mysite/verde/views.py
+++ mysite/verde/views.py
@@ -1,11 +1,21 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 import datetime
+from .models import EnergySource,ProducerType,GreenHouseGases,State
 
 #
 def index(request):
-     # return HttpResponse("Hello, world. You're at the verde.")
-    now = datetime.datetime.now()
-
+    sources = EnergySource.objects.all()
+    out_sources = []
+    for source in sources:
+        out_sources.append(source)
+    print(out_sources)
+    ptypes = ProducerType.objects.all()
+    out_types = []
+    for ptype in types:
+        out_types.append(ptypes)
+    print(out_types)
     # return HttpResponse(html)
-    return render(request, 'verde/index.html')
+    return render(request, 'verde/index2.html',{'EnergySource':out_sources},{'ProducerType': out_types})
+
+
